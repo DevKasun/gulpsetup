8fb19ca9d6221dabee873bf18e5c2f6c921b64f8
DevKasun <kasun2014@outlook.com>
2019-01-02 10:18:06 +0530

    First Commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var minifyjs = require('gulp-uglify');
+var uglifyjs = require('gulp-uglifyjs');
 
 // Compile SCSS to CSS
 gulp.task('styles', function() {
@@ -18,8 +20,16 @@ gulp.task('minifyStyles', function() {
 		.pipe(gulp.dest('./css/dist'));
 });
 
+// Minify JavaScript
+gulp.task('minifyJs', function() {
+	return gulp.src('./js/*.js')
+		.pipe(uglifyjs())
+		.pipe(rename('main.min.js'))
+		.pipe(gulp.dest('./js/dist'));
+});
+
 //Watch task
 gulp.task('watch',function() {
 	//console.log("It works");
-    gulp.watch('sass/*.scss', gulp.series('styles', 'minifyStyles'));
+    gulp.watch('sass/*.scss', gulp.series('styles', 'minifyStyles', 'minifyJs'));
 });
